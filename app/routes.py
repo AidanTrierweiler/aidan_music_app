074a319124154a84a787c280e9e12a5974918823
Aidan Trierweiler <atrierweiler@ithaca.edu>
2023-09-05 17:49:53 -0400

    everything for lab 2 and working

--- app/routes.py
+++ app/routes.py
@@ -5,17 +5,22 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home')
+
+
+@app.route('/artist')
+def artist():
+    artist_list = ["Jack Johnson", "Caamp", "Mt. Joy", "Dayglow"]
+    return render_template("artist.html", title="Artist", artist_list=artist_list)
+
+
+@app.route('/caamp')
+def artist_page():
+    return render_template("caamp.html", title="Caamp",)
+
+
+@app.route('/new_artist')
+def new_artist():
+    return render_template("new_artist.html", title="New Artist")
 
 
