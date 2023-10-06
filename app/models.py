87b664a6b03d82acc9c0e8ea859cbbd27d441ace
Aidan Trierweiler <atrierweiler@ithaca.edu>
2023-10-06 13:47:09 -0400

    databases is running and tables are working
    reset_db route is not working tho

--- app/models.py
+++ app/models.py
@@ -1,37 +1,37 @@
-from datetime import datetime
+# from datetime import datetime
 from app import db
 
 
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    description = db.Column(db.String(400), index=True)
-    age = db.Column(db.String(64))
-    artisttoevents = db.relationship('ArtisttoEvent', backref='artist', lazy='dynamic')
+    name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    a2es = db.relationship('ArtistToEvent', backref='artist', lazy='dynamic')
 
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    date = db.Column(db.String(64))
-    city = db.Column(db.String(64))
-    event_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    artisttoevents = db.relationship('ArtisttoEvent', backref='event', lazy='dynamic')
+    name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    date = db.Column(db.String(64), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
+    artists = db.relationship('Artist', backref='events', lazy='dynamic')
+    a2es = db.relationship('ArtistToEvent', backref='event', lazy='dynamic')
 
 
 class Venue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    city = db.Column(db.String(64))
-    size = db.Column(db.Integer)
-    events = db.relationship('Event', backref='event', lazy='dynamic')
+    name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    location = db.Column(db.String(64), nullable=False)
+    events = db.relationship('Event', backref='venue', lazy='dynamic')
 
 
-class ArtisttoEvent(db.Model):
+class ArtistToEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
 
 
 
-
+#
+# a1 = Artist.query.find(0)
+#
+# a1.a2es[0].event.venue.name
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, flash
-from app import app
+from app.models import Artist, Event, Venue
+from app import app, db
 from app.forms import NewArtistForm
 
 
@@ -30,3 +31,40 @@ def new_artist():
                                description=form.Description.data, hometown=form.hometown.data)
     return render_template('new_artist.html', title='New Artist', form=form, name=form.artist_name.data,
                            description=form.Description.data, hometown=form.hometown.data)
+
+
+@app.route('/reset_db')
+def reset_db():
+    flash("Resetting database: deleting old data and repopulating with dummy data")
+    # clear all data from all tables
+    meta = db.metadata
+    for table in reversed(meta.sorted_tables):
+        print('Clear table {}'.format(table))
+        db.session.execute(table.delete())
+    db.session.commit()
+
+    a1 = Artist(name="Caamp")
+    a2 = Artist(name="Lil Uzi")
+    a3 = Artist(name="Noah Kahan")
+    a4 = Artist(name="Jack Johnson")
+    a5 = Artist(name="Mt. Joy")
+    db.session.add_all([a1, a2, a3, a4, a5])
+    db.session.commit()
+
+    e1 = Event(name="Ozzfest", date="12/4/2023")
+    e2 = Event(name="Knotfest", date="12/10/2023")
+    e3 = Event(name="Rolling loud", date="12/25/2023")
+    e4 = Event(name="Coachella", date="1/4/2024")
+    e5 = Event(name="Extravaganza", date="1/10/2024")
+    e6 = Event(name="Bayfest", date="2/4/2024")
+    e7 = Event(name="Warped Tour", date="4/5/2024")
+    db.session.add_all([e1, e2, e3, e4, e5, e6, e7])
+    db.session.commit()
+
+    v1 = Venue(name="partyplace", location="New York")
+    v2 = Venue(name="jazzy Joy", location="Memphis")
+    v3 = Venue(name="Cool Venue", location="Boston")
+    db.session.add_all([v1, v2, v3])
+    db.session.commit()
+
+    return render_template('base.html', title='Home')
