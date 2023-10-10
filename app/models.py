b6c18970f05987ed1bcb5721a531fc2d3dab5930
Aidan Trierweiler <atrierweiler@ithaca.edu>
2023-10-10 13:21:17 -0400

    lab 5 done

--- app/models.py
+++ app/models.py
@@ -5,7 +5,12 @@ from app import db
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    description = db.Column(db.String(300))
+    hometown = db.Column(db.String(128))
     a2es = db.relationship('ArtistToEvent', backref='artist', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Artist {} - {}>'.format(self.id, self.name)
 
 
 class Event(db.Model):
@@ -13,8 +18,10 @@ class Event(db.Model):
     name = db.Column(db.String(64), index=True, unique=True, nullable=False)
     date = db.Column(db.String(64), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
-    artists = db.relationship('Artist', backref='events', lazy='dynamic')
     a2es = db.relationship('ArtistToEvent', backref='event', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Event {} - {}>'.format(self.id, self.name)
 
 
 class Venue(db.Model):
@@ -22,6 +29,9 @@ class Venue(db.Model):
     name = db.Column(db.String(64), index=True, unique=True, nullable=False)
     location = db.Column(db.String(64), nullable=False)
     events = db.relationship('Event', backref='venue', lazy='dynamic')
+
+    def __repr__(self):
+        return '<venue {} - {}>'.format(self.id, self.name)
 
 
 class ArtistToEvent(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, redirect, url_for, flash
-from app.models import Artist, Event, Venue
+from flask import render_template, redirect, url_for, flash, current_app, request
+from app.models import Artist, Event, Venue, ArtistToEvent
 from app import app, db
 from app.forms import NewArtistForm
 
@@ -10,27 +10,46 @@ def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/artist')
-def artist():
-    artist_list = ["Jack Johnson", "Caamp", "Mt. Joy", "Dayglow"]
-    return render_template("artist.html", title="Artist", artist_list=artist_list)
+@app.route('/artists')
+def artists():
+    artist_list = Artist.query.all()
+    return render_template("artist_list.html", artist_list=artist_list)
 
 
-@app.route('/caamp')
-def artist_page():
-    return render_template("caamp.html", title="Caamp", )
+@app.route('/artist/<name>')
+def artist_name(name):
+    artist = Artist.query.filter_by(name=name).first()
+
+    if artist:
+        return render_template("artist.html", artist=artist)
+    else:
+        flash("Artist '{name}' not found.", 'warning')
+        return redirect(url_for('index'))
 
 
 @app.route('/new_artist', methods=['GET', 'POST'])
 def new_artist():
     form = NewArtistForm()
+
     if form.validate_on_submit():
-        flash('New artist has been created'.format(
-            form.artist_name.data, form.hometown.data, form.Description.data))
-        return render_template('new_artist.html', title='New Artist', form=form, name=form.artist_name.data,
-                               description=form.Description.data, hometown=form.hometown.data)
-    return render_template('new_artist.html', title='New Artist', form=form, name=form.artist_name.data,
-                           description=form.Description.data, hometown=form.hometown.data)
+        artist_name = form.artist_name.data
+        artist_description = form.Description.data
+        artist_hometown = form.hometown.data
+
+        existing_artist = Artist.query.filter_by(name=artist_name).first()
+
+        if existing_artist:
+            flash(f"Artist '{artist_name}' already exists in the database.")
+        else:
+            # Create a new artist if it doesn't exist
+            new_artist = Artist(name=artist_name, description=artist_description, hometown=artist_hometown)
+            db.session.add(new_artist)
+            db.session.commit()
+            flash(f"Artist '{artist_name}' has been created.")
+
+        return redirect(url_for('artists'))
+
+    return render_template('new_artist.html', form=form)
 
 
 @app.route('/reset_db')
@@ -43,22 +62,12 @@ def reset_db():
         db.session.execute(table.delete())
     db.session.commit()
 
-    a1 = Artist(name="Caamp")
-    a2 = Artist(name="Lil Uzi")
-    a3 = Artist(name="Noah Kahan")
-    a4 = Artist(name="Jack Johnson")
-    a5 = Artist(name="Mt. Joy")
+    a1 = Artist(name="Caamp", hometown="Columbus Ohio", description="He loves playing folk music")
+    a2 = Artist(name="Lil Uzi", hometown="Philly", description="Rapper that makes good music")
+    a3 = Artist(name="Noah Kahan", hometown="Stafford Vermont", description="Makes good folk music")
+    a4 = Artist(name="Jack Johnson", hometown="Hawaii", description="Makes the best music of all time. Curious George!")
+    a5 = Artist(name="Mt. Joy", hometown="Philly", description="Makes soothing music")
     db.session.add_all([a1, a2, a3, a4, a5])
-    db.session.commit()
-
-    e1 = Event(name="Ozzfest", date="12/4/2023")
-    e2 = Event(name="Knotfest", date="12/10/2023")
-    e3 = Event(name="Rolling loud", date="12/25/2023")
-    e4 = Event(name="Coachella", date="1/4/2024")
-    e5 = Event(name="Extravaganza", date="1/10/2024")
-    e6 = Event(name="Bayfest", date="2/4/2024")
-    e7 = Event(name="Warped Tour", date="4/5/2024")
-    db.session.add_all([e1, e2, e3, e4, e5, e6, e7])
     db.session.commit()
 
     v1 = Venue(name="partyplace", location="New York")
@@ -67,4 +76,43 @@ def reset_db():
     db.session.add_all([v1, v2, v3])
     db.session.commit()
 
-    return render_template('base.html', title='Home')
+    e1 = Event(name="Ozzfest", date="12/4/2023", venue=v2)
+    e2 = Event(name="Knotfest", date="12/10/2023", venue=v1)
+    e3 = Event(name="Rolling loud", date="12/25/2023", venue=v2)
+    e4 = Event(name="Coachella", date="1/4/2024", venue=v3)
+    e5 = Event(name="Extravaganza", date="1/10/2024", venue=v1)
+    e6 = Event(name="Bayfest", date="2/4/2024", venue=v1)
+    e7 = Event(name="Warped Tour", date="4/5/2024", venue=v3)
+    db.session.add_all([e1, e2, e3, e4, e5, e6, e7])
+    db.session.commit()
+
+    a2es_list = [
+        ArtistToEvent(artist=a1, event=e1),
+        ArtistToEvent(artist=a3, event=e1),
+        ArtistToEvent(artist=a5, event=e1),
+
+        ArtistToEvent(artist=a2, event=e2),
+        ArtistToEvent(artist=a5, event=e2),
+        ArtistToEvent(artist=a4, event=e2),
+
+        ArtistToEvent(artist=a3, event=e3),
+        ArtistToEvent(artist=a5, event=e3),
+        ArtistToEvent(artist=a1, event=e3),
+
+        ArtistToEvent(artist=a2, event=e4),
+        ArtistToEvent(artist=a4, event=e4),
+
+        ArtistToEvent(artist=a2, event=e5),
+        ArtistToEvent(artist=a5, event=e5),
+        ArtistToEvent(artist=a4, event=e5),
+
+        ArtistToEvent(artist=a1, event=e6),
+        ArtistToEvent(artist=a2, event=e6),
+        ArtistToEvent(artist=a3, event=e6),
+
+        ArtistToEvent(artist=a2, event=e7)]
+
+    db.session.add_all(a2es_list)
+    db.session.commit()
+
+    return render_template('index.html', title='Home')
